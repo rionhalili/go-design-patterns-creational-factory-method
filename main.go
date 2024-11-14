1a2e8446441116f7d41d6cba7ecda4efbb05ae72
Rion Halili <rionhalili@gmail.com>
2024-11-15 00:11:56 +0100

    Implement main and display reports

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Hello")
+	appleCake, _ := getCake("Apple")
+	orangeCake, _ := getCake("Orange")
+
+	fmt.Println("Displayed cakes:")
+	fmt.Println("====================================")
+	report(appleCake)
+	report(orangeCake)
+	fmt.Println("====================================")
+}
+
+func report(cake ICake) {
+	fmt.Printf(
+		"Requested cake: %s is categorized as %s cake and costs %.2f",
+		cake.getName(),
+		cake.getCategory(),
+		cake.getPrice(),
+	)
+	fmt.Println()
 }
